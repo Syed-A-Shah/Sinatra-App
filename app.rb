f7b9c73d467c03aaf1fbfdf65bd02a3c98c1bb1f
Student <student@makerss-mbp-6.home>
2019-06-12 14:11:45 +0100

    Added form + posted params

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'shotgun'
 set :session_secret, 'super secret'
 
 get '/' do
-  "Hello"
+  "Hello!"
 end
 
 get '/secret' do
@@ -15,8 +15,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
